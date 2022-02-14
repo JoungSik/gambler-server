0cf0035c192dde7fb5dc14c37c5e6b8c28816ecb
JoungSik <tjstlr2010@gmail.com>
2022-02-15 08:16:23 +0900

    changed: discord Oauth 적용을 위한 칼럼 수정

--- db/migrate/20220214081324_devise_create_users.rb
+++ db/migrate/20220214081324_devise_create_users.rb
@@ -2,9 +2,10 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
+    create_table :users, id: false do |t|
+      t.bigint :id, null: false
       t.string :name, null: false
-      t.string :client_id, null: false
+      
       ## Database authenticatable
       t.string :email, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -37,10 +38,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.timestamps null: false
     end
 
-    add_index :users, :client_id, unique: true
+    add_index :users, :id, unique: true
     add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+
+    execute "ALTER TABLE users ADD PRIMARY KEY (id);"
   end
 end
